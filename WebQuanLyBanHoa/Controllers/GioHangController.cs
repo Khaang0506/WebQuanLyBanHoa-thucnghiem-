75084d338d290c44c581ef6713e494a66860138b
Nguyễn Hoàng Khang <nhk06052001@gmail.com>
2021-12-05 15:38:19 +0700

    Add project files.

---
+++ WebQuanLyBanHoa/Controllers/GioHangController.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebQuanLyBanHoa.Models;
+
+namespace WebQuanLyBanHoa.Controllers
+{
+    public class GioHangController : Controller
+    {
+        QuanLyBanHoaDataContext db = new QuanLyBanHoaDataContext();
+        //Lấy giỏ hàng
+        public List<ItemGioHang> LayGioHang()
+        {
+            List<ItemGioHang> lstGioHang = Session["GioHang"] as List<ItemGioHang>;
+            if (lstGioHang == null)//Nếu session giỏ hàng không tồn tại
+            {
+                lstGioHang = new List<ItemGioHang>();
+                Session["GioHang"] = lstGioHang;
+                return lstGioHang;
+            }
+            return lstGioHang;
+        }
+        //Thêm giỏ hàng thông thường (load lại trang)
+        public ActionResult ThemGioHang(int MaSP, string strURL)
+        {
+            //kiểm tra xem sản phẩm có tồn tại trong csdl hay không
+            SanPham sp = db.SanPhams.SingleOrDefault(x => x.MaSP == MaSP);
+            if (sp == null)
+            {
+                //Trang đường dẫn không hợp lệ
+                Response.StatusCode = 404;
+                return null;
+            }
+            //Lấy giỏ hàng
+            List<ItemGioHang> lstGioHang = LayGioHang();
+            //Trường hợp sản phẩm đã tồn tại trong giỏ hàng
+            ItemGioHang spCheck = lstGioHang.SingleOrDefault(x => x.MaSP == MaSP);
+            if (spCheck != null)
+            {
+                if (sp.SoLuongTon <= spCheck.SoLuong)
+                {
+                    return View("ThongBao");
+                }
+                spCheck.SoLuong++;
+                spCheck.ThanhTien = spCheck.SoLuong * spCheck.DonGia;
+                return Redirect(strURL);
+            }
+            
+            ItemGioHang itemGH = new ItemGioHang(MaSP);
+            itemGH.SoLuong++;
+            itemGH.ThanhTien = itemGH.SoLuong * itemGH.DonGia;
+            if (sp.SoLuongTon <= itemGH.SoLuong)
+            {
+                return View("ThongBao");
+            }
+            lstGioHang.Add(itemGH);
+            return Redirect(strURL);
+        }
+        //Tính tổng số lượng
+        public double TinhTongSoLuong()
+        {
+            List<ItemGioHang> lstGioHang = Session["GioHang"] as List<ItemGioHang>;
+            if (lstGioHang == null)
+            {
+                return 0;
+            }
+            return lstGioHang.Sum(x=>x.SoLuong);
+        }
+        //Tính tổng tiền
+        public decimal TinhTongTien()
+        {
+            List<ItemGioHang> lstGioHang = Session["GioHang"] as List<ItemGioHang>;
+            if (lstGioHang == null)
+            {
+                return 0;
+            }
+            return lstGioHang.Sum(x => x.ThanhTien);
+        }
+        public ActionResult GioHangPartial()
+        {
+            if (TinhTongSoLuong() == 0)
+            {
+                ViewBag.TongSoLuong = 0;
+                ViewBag.TongTien = 0;
+                return PartialView();
+            }
+            ViewBag.TongSoLuong = TinhTongSoLuong();
+            ViewBag.TongTien = TinhTongTien();
+            return PartialView();
+        }
+
+        // GET: GioHang
+        public ActionResult XemGioHang()
+        {
+            List<ItemGioHang> lstGioHang = LayGioHang();
+            ViewBag.TongSoLuong = TinhTongSoLuong();
+            ViewBag.TongTien = TinhTongTien();
+            return View(lstGioHang);
+        }
+
+        //Chỉnh sửa giỏ hàng
+        public ActionResult SuaGioHang(int MaSP)
+        {
+            //Kiểm tra session giỏ hàng tồn tại chưa
+            if (Session["GioHang"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            //kiểm tra xem sản phẩm có tồn tại trong csdl hay không
+            SanPham sp = db.SanPhams.SingleOrDefault(x => x.MaSP == MaSP);
+            if (sp == null)
+            {
+                //Trang đường dẫn không hợp lệ
+                Response.StatusCode = 404;
+                return null;
+            }
+            //Lấy list giỏ hàng từ session
+            List<ItemGioHang> lstGioHang = LayGioHang();
+            //Kiểm tra xem sản phẩm đó có tồn tại trong giỏ hàng hay không
+            ItemGioHang spCheck = lstGioHang.SingleOrDefault(x => x.MaSP == MaSP);
+            if (spCheck == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            //Lấy list giỏ hàng
+            ViewBag.GioHang = lstGioHang;
+            //Nếu sp tồn tại
+            return View(spCheck);
+        }
+        //Xử lý cập nhật giỏ hàng
+        [HttpPost]
+        public ActionResult CapNhatGioHang(ItemGioHang itemGH)
+        {
+            //Kiểm tra số lượng tồn
+            SanPham spCheck = db.SanPhams.SingleOrDefault(n => n.MaSP == itemGH.MaSP);
+            if(spCheck.SoLuongTon < itemGH.SoLuong)
+            {
+                return View("ThongBao");
+            }
+            //Cập nhật số lượng trong session giỏ hàng
+            //Bước 1: Lấy List<GioHang> từ session["GioHang"]
+            List<ItemGioHang> lstGH = LayGioHang();
+            //Bước 2: Lấy sản phẩm cần cập nhật từ trong List<GioHang> ra
+            ItemGioHang itemGHUpdate = lstGH.Find(x => x.MaSP == itemGH.MaSP);
+            //Bước 3: Cập nhật lại số lượng và thành tiền
+            itemGHUpdate.SoLuong = itemGH.SoLuong;
+            itemGHUpdate.ThanhTien = itemGHUpdate.SoLuong * itemGHUpdate.DonGia;
+            return RedirectToAction("XemGioHang");
+        }
+        public ActionResult XoaGioHang(int MaSP)
+        {
+            //Kiểm tra session giỏ hàng tồn tại chưa
+            if (Session["GioHang"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            //kiểm tra xem sản phẩm có tồn tại trong csdl hay không
+            SanPham sp = db.SanPhams.SingleOrDefault(x => x.MaSP == MaSP);
+            if (sp == null)
+            {
+                //Trang đường dẫn không hợp lệ
+                Response.StatusCode = 404;
+                return null;
+            }
+            //Lấy list giỏ hàng từ session
+            List<ItemGioHang> lstGioHang = LayGioHang();
+            //Kiểm tra xem sản phẩm đó có tồn tại trong giỏ hàng hay không
+            ItemGioHang spCheck = lstGioHang.SingleOrDefault(x => x.MaSP == MaSP);
+            if (spCheck == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            //Xoá sản phẩm trong giỏ hàng
+            lstGioHang.Remove(spCheck);
+            return RedirectToAction("XemGioHang");
+        }
+        //Xây dựng chức năng đặt hàng
+        public ActionResult DatHang(KhachHang kh)
+        {
+            //Kiểm tra session giỏ hàng tồn tại chưa
+            if (Session["GioHang"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            KhachHang khachHang = new KhachHang();
+            if (Session["TaiKhoan"] == null)
+            {
+                //Thêm một khách hàng mới vào csdl
+                khachHang = kh;
+                db.KhachHangs.InsertOnSubmit(khachHang);
+                db.SubmitChanges();
+            }
+            else
+            {
+                //Đối với khách hàng là thành viên
+                ThanhVien tv = Session["TaiKhoan"] as ThanhVien;
+                khachHang.TenKH = tv.HoTen;
+                khachHang.DiaChi = tv.DiaChi;
+                khachHang.Email = tv.Email;
+                khachHang.SoDienThoai = tv.SoDienThoai;
+                khachHang.MaThanhVien = tv.MaThanhVien;
+                db.KhachHangs.InsertOnSubmit(khachHang);
+                db.SubmitChanges();
+            }
+            //Thêm đơn hàng
+            DonDatHang ddh = new DonDatHang();
+            ddh.MaKH = khachHang.MaKH;
+            ddh.NgayDat = DateTime.Now;
+            ddh.TinhTrangDonHang = false;
+            ddh.DaThanhToan = false;
+            ddh.UuDai = 0;
+            ddh.DaHuy = false;
+            ddh.DaXoa = false;
+            db.DonDatHangs.InsertOnSubmit(ddh);
+            db.SubmitChanges();
+            //Thêm chi tiết đơn đặt hàng
+            List<ItemGioHang> lstGH = LayGioHang();
+            
+            foreach(var item in lstGH)
+            {
+                ChiTietDonDatHang ctdh = new ChiTietDonDatHang();
+                ctdh.MaDDH = ddh.MaDDH;
+                ctdh.MaSP = item.MaSP;
+                ctdh.TenSP = item.TenSP;
+                ctdh.SoLuong = item.SoLuong;
+                ctdh.DonGia = item.DonGia;
+                db.ChiTietDonDatHangs.InsertOnSubmit(ctdh);
+                //Cập nhật lại số lượng tồn
+                SanPham sp = db.SanPhams.SingleOrDefault(x => x.MaSP == item.MaSP);
+                if (sp.MaSP == item.MaSP)
+                {
+                    sp.SoLuongTon = sp.SoLuongTon - item.SoLuong;
+                    db.SubmitChanges();
+                }
+
+            }
+            db.SubmitChanges();
+            Session["GioHang"] = null;
+            return View("ThongBaoThanhCong");
+            //return RedirectToAction("XemGioHang");
+        }
+        //Thêm giỏ hàng ajax
+        public ActionResult ThemGioHangAjax(int MaSP, string strURL)
+        {
+            //kiểm tra xem sản phẩm có tồn tại trong csdl hay không
+            SanPham sp = db.SanPhams.SingleOrDefault(x => x.MaSP == MaSP);
+            if (sp == null)
+            {
+                //Trang đường dẫn không hợp lệ
+                Response.StatusCode = 404;
+                return null;
+            }
+            //Lấy giỏ hàng
+            List<ItemGioHang> lstGioHang = LayGioHang();
+            //Trường hợp sản phẩm đã tồn tại trong giỏ hàng
+            ItemGioHang spCheck = lstGioHang.SingleOrDefault(x => x.MaSP == MaSP);
+            if (spCheck != null)
+            {
+                if (sp.SoLuongTon <= spCheck.SoLuong)
+                {
+                    return Content("<script>alert(\"Sản phẩm đã hết hàng!\")</script>");
+                }
+                spCheck.SoLuong++;
+                spCheck.ThanhTien = spCheck.SoLuong * spCheck.DonGia;
+                ViewBag.TongSoLuong = TinhTongSoLuong();
+                ViewBag.TongTien = TinhTongTien();
+                return PartialView("GioHangPartial");
+            }
+
+            ItemGioHang itemGH = new ItemGioHang(MaSP);
+            if (sp.SoLuongTon <= itemGH.SoLuong)
+            {
+                return View("ThongBao");
+            }
+            lstGioHang.Add(itemGH);
+            return Redirect(strURL);
+        }
+    }
+}
---
+++ WebQuanLyBanHoa/Controllers/HomeController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebQuanLyBanHoa.Models;
+using CaptchaMvc.HtmlHelpers;
+using CaptchaMvc;
+using System.Web.Security;
+
+namespace WebQuanLyBanHoa.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        // GET: Home
+        QuanLyBanHoaDataContext db = new QuanLyBanHoaDataContext();
+        public ActionResult Index()
+        {
+            //tạo viewbag lấy list sản phẩm mới
+            var lstMoi = db.SanPhams.Where(x => x.Moi == 1 && x.DaXoa == false);
+            ViewBag.lsMoi = lstMoi;
+            //tạo viewbag list sản phẩm bán chạy
+            var lstBanChay = db.SanPhams.Where(x => x.Moi == 2 && x.DaXoa == false);
+            ViewBag.lsBC = lstBanChay;
+            return View();
+        }
+        public ActionResult MenuPartial()
+        {
+            var lstSP = db.SanPhams;
+            return PartialView(lstSP);
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy(ThanhVien tv)
+        {
+            //Kiểm tra captcha hợp lệ
+            if(this.IsCaptchaValid("Captcha is not valid"))
+            {
+                if (ModelState.IsValid) { 
+                ViewBag.ThongBao = "Đăng kí thành công";
+                //thêm khách hàng vào csdl
+                db.ThanhViens.InsertOnSubmit(tv);
+                db.SubmitChanges();
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Đăng kí thất bại";
+                }
+                return View();
+            }
+            ViewBag.ThongBao = "Sai mã Captcha";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection f)
+        {
+            //string sTaiKhoan = f["txtTenDangNhap"].ToString();
+            //string sMatKhau = f["txtMatKhau"].ToString();
+
+            //ThanhVien tv = db.ThanhViens.SingleOrDefault(x => x.TaiKhoan == sTaiKhoan && x.MatKhau == sMatKhau);
+            //if(tv != null)
+            //{
+            //    Session["TaiKhoan"] = tv;
+            //    return Content("<script>window.location.reload();</script>");
+            //}
+
+            //return Content("Tài khoản hoặc mật khẩu không đúng!");
+            string taikhoan = f["txtTenDangNhap"].ToString();
+            string matkhau = f["txtMatKhau"].ToString();
+            //Truy vấn kiểm tra đăng nhập lấy thông tin thành viên
+            ThanhVien tv = db.ThanhViens.SingleOrDefault(x => x.TaiKhoan == taikhoan && x.MatKhau == matkhau);
+            if (tv != null)
+            {
+                Session["TaiKhoan"] = tv;
+                //lấy ra list quyền thành viên tương ứng với loại
+                var lstQuyen = db.LoaiThanhVien_Quyens.Where(x=>x.MaLoaiTV==tv.MaLoaiTV);
+                //Duyệt list quyền
+                string Quyen = "";
+                if (lstQuyen.Count() != 0)
+                {
+                    foreach (var item in lstQuyen)
+                    {
+                        Quyen += item.Quyen.MaQuyen + ",";
+                    }
+                    Quyen = Quyen.Substring(0, Quyen.Length - 1);//Cắt đi dấu phẩy
+                    PhanQuyen(tv.TaiKhoan.ToString(), Quyen);
+                }
+                return Content("<script>window.location.reload();</script>");
+            }
+            return Content("Tài khoản hoặc mật khẩu không đúng!");
+        }
+        public void PhanQuyen(string TaiKhoan, string Quyen)
+        {
+            FormsAuthentication.Initialize();
+            var ticket = new FormsAuthenticationTicket(1,
+                                TaiKhoan,//user
+                                DateTime.Now,//begin
+                                DateTime.Now.AddHours(3),//time out
+                                false,//remember?
+                                Quyen,
+                                FormsAuthentication.FormsCookiePath);
+            var cookie = new HttpCookie(FormsAuthentication.FormsCookieName,
+                                FormsAuthentication.Encrypt(ticket));
+            if (ticket.IsPersistent) cookie.Expires = ticket.Expiration;
+
+            Response.Cookies.Add(cookie);
+        }
+        //Tạo trang ngăn chặn quyền truy cập
+        public ActionResult LoiPhanQuyen()
+        {
+            return View();
+        }
+        public ActionResult DangXuat()
+        {
+            Session["TaiKhoan"] = null;
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+
+        //Giải phóng biến cho vùng nhớ
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (db != null)
+                    db.Dispose();
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
